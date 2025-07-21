c4d989e4ae7c22b67b3cc11cf24ec4d6c28920d3
Alejandro Robles <b_re3@hotmail.com>
2025-07-21 15:25:14 -0600

    bye

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-# Usar la imagen oficial de MySQL
-FROM mysql:8.0
-
-# Copiar los scripts de inicialización al directorio correcto dentro del contenedor
-COPY ./mysql-init /docker-entrypoint-initdb.d/
--- mysql-init/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS nocodb
-CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-GRANT ALL PRIVILEGES ON nocodb.* TO 'nocodb_mysql_user'@'%';
-
--- Asegurar que los privilegios sean aplicados
-FLUSH PRIVILEGES;
