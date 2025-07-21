41164c3e4b9e1614e74bd285765391f3c541b4b6
Alejandro Robles <b_re3@hotmail.com>
2025-07-21 14:45:38 -0600

    mysql user

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,3 +1,7 @@
 CREATE DATABASE IF NOT EXISTS nocodb
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 GRANT ALL PRIVILEGES ON nocodb.* TO 'nocodb_mysql_user'@'%';
+
+-- Asegurar que los privilegios sean aplicados
+FLUSH PRIVILEGES;
